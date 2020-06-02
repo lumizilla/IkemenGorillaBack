e905a32ec037566e1ae8360aef1f383cc260ca4d
Luiza Culau <luizaculau@Luizas-MacBook-Air.local>
2020-06-02 15:31:28 +0900

    latest changes

--- app.py
+++ app.py
@@ -66,13 +66,13 @@ def testdatabase():
 
     # Return the response in json format
     return jsonify(response)
-    
+
 #-------------------------------------------------------------
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to our IkemenGorilla server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
