9044dad55f397efe8bab50f42f6aa4cfb532bd6d
Luiza Culau <luizaculau@Luizas-MacBook-Air.local>
2020-06-03 15:58:07 +0900

    adding recommended zoos function

--- app.py
+++ app.py
@@ -140,6 +140,9 @@ DATABASE = 'ikemengori.db'
 
 def get_db():
     db = getattr(g, '_database', None)
+
+    db.row_factory = sqlite3.Row
+
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
