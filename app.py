1e2fe61852b916dabe4ad7be497b5228ff2f7120
Luiza Culau <luizaculau@Luizas-MacBook-Air.local>
2020-06-03 18:50:02 +0900

    finished zoos/recommended

--- app.py
+++ app.py
@@ -141,14 +141,9 @@ DATABASE = 'ikemengori.db'
 
 def get_db():
     db = getattr(g, '_database', None)
-    """
-    def make_dicts(cursor, row):
-        return dict((cursor.description[idx][0], value)
-            for idx, value in enumerate(row)) 
-    """
+
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-        #db.row_factory = make_dicts()
         db.row_factory = sqlite3.Row
     return db
 
