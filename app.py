9f391c528a1252dba2ffdd26b072f466c13591fd
Luiza Culau <luizaculau@Luizas-MacBook-Air.local>
2020-06-03 18:21:08 +0900

    trying to create row factory

--- app.py
+++ app.py
@@ -143,12 +143,11 @@ def get_db():
 
     def make_dicts(cursor, row):
         return dict((cursor.description[idx][0], value)
-            for idx, value in enumerate(row))
-
-    db.row_factory = make_dicts
+            for idx, value in enumerate(row)) 
 
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts()
     return db
 
 @app.teardown_appcontext
